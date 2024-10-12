3e26b4e7176abbba67650b47a72053b98a948d57
Shofiqur Rahman Soyon <shofiqur7684@gmail.com>
2024-10-13 00:58:35 +0600

    //divider //showModalBottom sheet // TextFiled

--- lib/text_filed.dart
+++ lib/text_filed.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //import 'package:untitled/main.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -16,23 +16,119 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class Home extends StatelessWidget{
+
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TExt'),
+        title: const Text('TExt'),
+        backgroundColor: Colors.red,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              onTap: (){
-
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    barrierColor: Colors.grey,
+                    context: context,
+                    builder: (ctx) {
+                      return Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                TextButton(
+                                    onPressed: () {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: Text('delete'),
+                                              content: Text('Are you sure?'),
+                                              actions: [
+                                                TextButton(onPressed: (){},
+                                                    child: Text('yes')
+                                                ),
+                                                TextButton(onPressed: (){},
+                                                    child: Text('no')
+                                                ),
+                                              ],
+                                            );
+                                          });
+                                    },
+                                    child: Text('title'))
+                              ],
+                            ),
+                          ),
+                          Divider(height: 10,),
+                          Text('Sample'),
+                          Row(
+                            children:[
+                              ElevatedButton(onPressed: (){}, child: Text('Cancel')),
+                              VerticalDivider(width: 20,thickness: 5,
+                                color: Colors.grey,
+                              ),
+                              ElevatedButton(onPressed: (){}, child: Text('Save')),
+                            ],
+                          ),
+                        ],
+                      );
+                    });
               },
+              child: Text('Show Dialog'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: TextField(
+                onTap: () {},
+                // obscureText:true,
+                maxLength: 11,
+                maxLines: 1,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  hintText: 'Phone',
+                  // labelText: 'Phone Number',
+                  prefixIcon: Icon(Icons.call),
+                  prefix: Text('+880 '),
+                  suffixIcon: Icon(Icons.contact_phone),
+                  fillColor: Colors.white24,
+                  filled: true,
+                  label: Text(
+                    'Phone Number',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+                  /* disabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),*/
+                ),
+              ),
             )
           ],
         ),
