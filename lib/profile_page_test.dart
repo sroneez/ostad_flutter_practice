9dd57c442792fd43c9ba3f7744599f6f4c1a5e43
Shofiqur Rahman Soyon <shofiqur7684@gmail.com>
2024-10-17 18:58:12 +0600

    creating insta profile page and added custom Icon library

--- lib/profile_page_test.dart
+++ lib/profile_page_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfilePageTest extends StatelessWidget {
   const ProfilePageTest({Key? key}) : super(key: key);
@@ -6,64 +7,228 @@ class ProfilePageTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Profile'),
-          centerTitle: true,
-          titleTextStyle: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-            color: Colors.black,
-          ),
-          leading:
-              IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
-          actions: [
-            const Text('Settings'),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
-          ],
+      appBar: AppBar(
+        title: const Text('Profile'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+          color: Colors.black,
         ),
-        body: Center(
+        leading:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+        actions: [
+          const Text('Settings'),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
           child: Column(
             children: [
-              SizedBox(height: 20,),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/face1.webp',),
-                // minRadius: 60,
-                maxRadius: 70,
+              SizedBox(
+                height: 20,
               ),
+              Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                    'assets/face1.webp',
+                  ),
+                  // minRadius: 60,
+                  maxRadius: 60,
+                ),
+                Positioned(
+                  right: 11,
+                  bottom: 11,
+                  child: Container(
+                    width: 12,
+                    height: 12,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white,width: 2),
+                    ),
+                  ),
+                ),
+              ]),
               const SizedBox(height: 10),
-              Text('Mark Zukerberg',
-                style:const TextStyle(
+              Text(
+                'Mark Zuckerberg',
+                style: const TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(height: 10,),
-              Text('Photography | Travelling | Programming',
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Photography | Travelling | Programming',
                 style: TextStyle(
-                  fontWeight: FontWeight.w300,
+                  fontWeight: FontWeight.w200,
                 ),
               ),
-              Text('California, USA',
+              Text(
+                'California, USA',
                 style: TextStyle(
-                  fontWeight: FontWeight.w300,
+                  fontWeight: FontWeight.w200,
                 ),
+              ),
+              const SizedBox(
+                height: 10,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(Icons.edit),
+                  Icon(
+                    Icons.edit,
+                    size: 14,
+                  ),
                   Text('Edit the profile'),
-                  const SizedBox(height:20),
+                  const SizedBox(height: 20),
                 ],
               ),
+              SizedBox(
+                height: 32,
+              ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text('Post'),
+                  Column(
+                    children: [
+                      Text(
+                        'Post',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w200,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('35',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text('Followers',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w200,
+                            fontSize: 16,
+                          )),
+                      Text(
+                        '4521',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text('Follows',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w200,
+                            fontSize: 16,
+                          )),
+                      Text('124',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.asset(
+                      'assets/face2.webp',
+                      height: 210,
+                      width: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.asset(
+                          'assets/face3.webp',
+                          width: 150,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      // Space between second and third images
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.asset(
+                          'assets/face4.webp',
+                          width: 150,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ],
           ),
-        )
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 3,
+        onTap: (index) {},
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              label: 'Search'),
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.paperPlane,
+                color: Colors.black,
+              ),
+              label: 'Share'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_border,
+                color: Colors.black,
+              ),
+              label: 'Like'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              label: 'Profile'),
+        ],
+      ),
     );
   }
 }
